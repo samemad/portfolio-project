7eee1cc946e218c410c2c88e1c40ac79d8b76202
samemad <alsamemad775@gmail.com>
2025-08-31 11:51:49 +0300

    Fix API baseURL to point to Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "https://portfolio-project-p04q.onrender.com/api"
 });
 
 // Existing functions
