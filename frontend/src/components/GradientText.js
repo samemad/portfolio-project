ba10bc8609768538ce587af066739d57dbcef4ee
samemad <alsamemad775@gmail.com>
2025-09-10 02:05:13 +0300

    Fix unused variable and add gradient text to all sections

--- frontend/src/components/GradientText.js
+++ frontend/src/components/GradientText.js
@@ -1,7 +1,7 @@
 export default function GradientText({
   children,
   className = '',
-  colors = ['#ffaa40', '#9c40ff', '#ffaa40'],
+  colors = ['#2b2b36, #1f1f2a'],
   animationSpeed = 8,
   showBorder = false
 }) {
